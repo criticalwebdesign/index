a8cd3e8df116179701348470444a6317b1bfc097
Owen Mundy <omundy@gmail.com>
2025-01-02 22:46:20 -0500

    Update tags and about to match js-rewrite

--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -4,13 +4,17 @@
 	// console.log(tag, note);
 	import { page } from '$app/stores';
 	import { tag, sortField, sortOrder, projectStore } from '$lib/stores/stores.js';
+		// show button active https://learn.svelte.dev/tutorial/classes
+		let active = false;
 </script>
 
 <button
 	class="tag"
+	class:active={$tag === val}
 	title={note}
 	data-tag={val}
 	on:click={(e) => {
+	console.log('current >', $tag, 'clicked > ', e.target.dataset.tag, val);
 		// console.log(e.target.dataset.tag);
 		window.location.hash = val;
 		// console.log($page);
@@ -22,3 +26,26 @@
 		{$sortOrder > 0 ? '↓' : '↑'}
 	{/if}
 </button>
+<style>
+	.tag {
+		background: var(--violet-12);
+		color: var(--indigo-5);
+		min-width: 2rem;
+		margin-right: 0.3rem;
+	}
+
+	.tag:hover {
+		background: var(--violet-10);
+		color: var(--indigo-3);
+	}
+
+	.tag.active {
+		background: var(--violet-8);
+		color: var(--indigo-2);
+	}
+
+	.tag.active:hover {
+		background: var(--violet-9);
+		color: var(--indigo-2);
+	}
+</style>
