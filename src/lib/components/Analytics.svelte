2d59946f24aace15ced11d23f889eba354095fe2
Owen Mundy <omundy@gmail.com>
2026-01-19 08:05:16 -0500

    Adds analytics

---
+++ src/lib/components/Analytics.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { browser } from '$app/environment';
+</script>
+
+<svelte:head>
+	{#if browser}
+
+		<script type="text/javascript">
+			var sc_project = 13199155;
+			var sc_invisible = 1;
+			var sc_security = '6c3904c9';
+		</script>
+		<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>
+
+	{/if}
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,11 @@
 	// @ts-nocheck
 
 	import Header from '$lib/components/Header.svelte';
+	import Analytics from '$lib/components/Analytics.svelte';
 </script>
 
 <Header />
+
+<Analytics />
 
 <slot></slot>
